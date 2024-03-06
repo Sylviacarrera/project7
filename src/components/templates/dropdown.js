54b2d5bc0d65499678d5fc3c426c419e6c22ac10
Sylviacarrera <129325236+Sylviacarrera@users.noreply.github.com>
2024-03-06 19:06:59 +0100

    filterstags

--- src/components/templates/dropdown.js
+++ src/components/templates/dropdown.js
@@ -9,7 +9,7 @@ export const createItem = (data, parent) => {
     parent.appendChild(article)
 
     const closeIconItem = document.createElement('i')
-    closeIconItem.classList.add('fas', 'fa-circle-xmark', 'close-icon') // Ajouter les classes pour l'icône de croix
+    closeIconItem.classList.add('fa-solid', 'fa-circle-xmark', 'close-icon') // Ajouter les classes pour l'icône de croix
     article.appendChild(closeIconItem)
 
     article.addEventListener('click', () => {
@@ -18,27 +18,19 @@ export const createItem = (data, parent) => {
     })
   })
 }
+
 // Fonction pour créer un tag à partir d'un élément de liste cliqué
 const createTag = (text, tagContainer) => {
   const tag = document.createElement('span')
-  tag.classList.add('tag')
-
-  const tagContent = document.createElement('span')
-  tagContent.textContent = text
-
-  const closeIcon = document.createElement('span')
-  closeIcon.textContent = '✖' // Ajoutez ici le symbole que vous souhaitez utiliser pour la croix
-  closeIcon.classList.add('close-icon')
-
-  // Ajouter un gestionnaire d'événements pour supprimer le tag lorsque la croix est cliquée
-  closeIcon.addEventListener('click', () => {
+  tag.className = 'tag'
+  tag.innerHTML = `
+    <span>${text}</span>
+    <span class="close-icon">✖</span>
+  `
+  tag.querySelector('.close-icon').addEventListener('click', () => {
     tag.remove()
     clickedArticles.delete(text)
   })
-
-  tag.textContent = text
-  tag.appendChild(closeIcon)
-
   tagContainer.appendChild(tag)
 }
 
@@ -48,24 +40,12 @@ const clickedArticles = new Set()
 // Fonction pour gérer le clic sur les éléments de liste
 const handleListClick = (listElement, tagContainer) => {
   listElement.addEventListener('click', event => {
-    const clickedItem = event.target
-    const clickedText = clickedItem.textContent
-
-    // Vérifier si le texte est vide
-    if (clickedText === '') {
-      return // Ne rien faire si le texte est vide
+    const clickedText = event.target.textContent.trim()
+    if (clickedText && !clickedArticles.has(clickedText)) {
+      clickedArticles.add(clickedText)
+      createTag(clickedText, tagContainer)
+      console.log('Element cliqué:', clickedText)
     }
-    // Vérifier si l'article a déjà été cliqué
-    if (clickedArticles.has(clickedText)) {
-      // Ignorer le clic si l'article a déjà été cliqué
-      return
-    }
-    // Ajouter l'article à la liste des articles cliqués
-    clickedArticles.add(clickedText)
-
-    // Créer un tag à partir du texte de l'article cliqué
-    createTag(clickedText, tagContainer)
-    console.log('Element cliqué:', clickedText)
   })
 }
 
